020591beebb423ea85588ab20be0db76b7f3f547
Scott <13770321+scott-8@users.noreply.github.com>
2022-08-11 16:13:17 -0400

    update package name

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://hero-static.s3.amazonaws.com/linkciter/";
+const urlProd = "https://hero-static.s3.amazonaws.com/litciter/";
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
